61dbf25b8f558ed80e401306839f7b5b3f90d53b
Waterpoweer <redaouardi6@gmail.com>
2026-03-14 19:12:43 -0400

    dominant_emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,13 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     payload =  { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, headers= header, json = payload )
+    dictionary = json.loads(response.text)
+    dict_emotion = dictionary["emotionPredictions"][0]["emotion"]
+    dict_dominant={"dominant_emotion": max(dict_emotion, key=dict_emotion.get)}
+    dict_emotion.update(dict_dominant)    
+    return dict_emotion
 
-    return response.text
